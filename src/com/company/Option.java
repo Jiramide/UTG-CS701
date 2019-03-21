007ab4f109f187f26fd985fb09354a12d2dac40b
Jade Piramide <jadepiramidepogi@gmail.com>
2019-03-20 19:09:48 -0700

    Added Option<T> class with the intent of adding safe variants for unsafe methods such as uncons, remove, etc.

---
+++ src/com/company/Option.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.function.Function;
+
+public class Option<T> {
+
+  public boolean hasValue;
+  public T val;
+
+  public Option() {
+    this.hasValue = false;
+    this.val = null;
+  }
+
+  public Option(T val) {
+    this.hasValue = true;
+    this.val = val;
+  }
+
+  public <U> Option<U> fmap(Function<T, U> f) {
+    return hasValue ? new Option<>(f.apply(val)) : new Option<>();
+  }
+
+  public <U> Option<U> bind(Function<T, Option<U>> f) {
+    return hasValue ? f.apply(val) : new Option<>();
+  }
+
+  public <U> U maybe(Function<T, U> f, U def) {
+    return hasValue ? f.apply(val) : def;
+  }
+
+  public static <T> T fromOption(Option<T> opt, T def) {
+    return opt.hasValue ? opt.val : def;
+  }
+}
