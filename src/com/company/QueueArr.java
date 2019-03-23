ab1893c4a14f5337b485d426d81b4cb69068ebe2
Jade Piramide <jadepiramidepogi@gmail.com>
2019-03-23 01:04:27 -0700

    Completed QueueArr (part 2 because I didn't stage it the first time).

--- src/com/company/QueueArr.java
+++ src/com/company/QueueArr.java
@@ -1,10 +1,12 @@
 package com.company;
 
+import java.lang.RuntimeException;
+
 public class QueueArr<E> {
 
   private E[] container;
-  private int enqueueIdx;
-  private int dequeueIdx;
+  private int enqueueIdx; // points to the idx to write
+  private int dequeueIdx; // points to the idx to read
   private int capacity;
 
   public QueueArr(Class<E> cls, int capacity) {
@@ -15,7 +17,54 @@ public class QueueArr<E> {
     this.enqueueIdx = 0;
     this.dequeueIdx = 0;
     this.capacity = capacity;
+  }
 
+  public void enqueue(E val) {
+    if (isFull()) {
+      throw new RuntimeException("QueueArr: queue overflow");
+    }
+
+    container[enqueueIdx++ % capacity] = val;
+  }
+
+  public E dequeue() {
+    if (isEmpty()) {
+      throw new RuntimeException("QueueArr: queue underflow");
+    }
+
+    return container[dequeueIdx++ % capacity];
+  }
+
+  public Option<E> safeDequeue() {
+    return !isEmpty()
+      ? new Option<>(dequeue())
+      : new Option<>();
+  }
+
+  public E peek() {
+    if (isEmpty()) {
+      throw new RuntimeException("QueueArr: queue underflow");
+    }
+
+    return container[dequeueIdx % capacity];
+  }
+
+  public Option<E> safePeek() {
+    return !isEmpty()
+      ? new Option<>(peek())
+      : new Option<>();
+  }
+
+  public int numItems() {
+    return enqueueIdx - dequeueIdx;
+  }
+
+  public boolean isEmpty() {
+    return numItems() <= 0;
+  }
+
+  public boolean isFull() {
+    return numItems() >= capacity;
   }
 
 }
