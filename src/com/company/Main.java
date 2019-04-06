bf58df43b84d6eccefc3950b7c7f38e62cf26b55
Jiramide <jadepiramidepogi@gmail.com>
2019-04-06 15:33:24 -0700

    Added recursive methods (factorial and fibonnaci)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,81 +6,104 @@ import java.lang.Math;
 
 public class Main {
 
+  public static int fact(int n) {
+    if (n == 0) {
+      return 1;
+    }
+
+    return n*fact(n - 1);
+  }
+
+  public static int fib(int n) {
+    if (n == 0 || n == 1) {
+      return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+  }
+
   public static void main(String[] args) {
 
-      System.out.println("Proper String");
-      Scanner in = new Scanner(System.in);
-      int iter = in.nextInt();
+    Scanner in = new Scanner(System.in);
 
-      for (; iter > 0; iter--) {
-        System.out.println(
-          ParenthesizedString.IsProper(in.next())
-            ? "Yes"
-            : "No"
-        );
+    System.out.println("Factorial");
+    System.out.println(fact(in.nextInt()));
+
+    System.out.println("Fibonnaci");
+    System.out.println(fib(in.nextInt()));
+
+    System.out.println("Proper String");
+    int iter = in.nextInt();
+
+    for (; iter > 0; iter--) {
+      System.out.println(
+        ParenthesizedString.IsProper(in.next())
+          ? "Yes"
+          : "No"
+      );
+    }
+
+    System.out.println("Longest Alimentary Chain");
+    int creatures;
+    int relations;
+
+    while ((creatures = in.nextInt()) != 0 & (relations = in.nextInt()) != 0) {
+      DisjointSet<String> creatureChain = new DisjointSet<>(creatures);
+
+      for (; creatures > 0; creatures--) {
+        creatureChain.add(in.next());
       }
 
-      System.out.println("Longest Alimentary Chain");
-      int creatures;
-      int relations;
-
-      while ((creatures = in.nextInt()) != 0 & (relations = in.nextInt()) != 0) {
-        DisjointSet<String> creatureChain = new DisjointSet<>(creatures);
-
-        for (; creatures > 0; creatures--) {
-          creatureChain.add(in.next());
-        }
-
-        for (; relations > 0 ; relations--) {
-          creatureChain.union(in.next(), in.next());
-        }
-
-        int biggestChain = 0;
-
-        for (Set<String> chain : creatureChain.sets()) {
-          biggestChain = Math.max(biggestChain, chain.size());
-        }
-
-        System.out.println(biggestChain);
+      for (; relations > 0 ; relations--) {
+        creatureChain.union(in.next(), in.next());
       }
 
-      System.out.println("Maximal Connected Subgraph");
-      int cases = in.nextInt();
+      int biggestChain = 0;
 
-      for (; cases > 0; cases--) {
-        DisjointSet<Character> graph = new DisjointSet<>();
-        char max = in.next().charAt(0);
-
-        for (char curr = 'A'; curr <= max; curr++) {
-          graph.add(curr);
-        }
-
-        in.nextLine();
-
-        String rel = in.nextLine();
-        while (!rel.trim().isEmpty()) {
-          graph.union(rel.charAt(0), rel.charAt(1));
-          rel = in.nextLine();
-        }
-
-        System.out.println(graph.numSets());
+      for (Set<String> chain : creatureChain.sets()) {
+        biggestChain = Math.max(biggestChain, chain.size());
       }
 
-      Integer[] arr = {1, 8, 5, 3, 0, 7, 2, 6, 9, 4};
-      System.out.println("Using Comparable(SelectionSort)");
-      System.out.print("Unsorted: ");
-      for (Integer i : arr) {
-        System.out.print(i);
-        System.out.print(" ");
+      System.out.println(biggestChain);
+    }
+
+    System.out.println("Maximal Connected Subgraph");
+    int cases = in.nextInt();
+
+    for (; cases > 0; cases--) {
+      DisjointSet<Character> graph = new DisjointSet<>();
+      char max = in.next().charAt(0);
+
+      for (char curr = 'A'; curr <= max; curr++) {
+        graph.add(curr);
       }
 
-      System.out.print("\nSorted:");
+      in.nextLine();
 
-      SelectionSort.sort(arr);
-      for (Integer i : arr) {
-        System.out.print(i);
-        System.out.print(" ");
+      String rel = in.nextLine();
+      while (!rel.trim().isEmpty()) {
+        graph.union(rel.charAt(0), rel.charAt(1));
+        rel = in.nextLine();
       }
+
+      System.out.println(graph.numSets());
+    }
+
+    Integer[] arr = {1, 8, 5, 3, 0, 7, 2, 6, 9, 4};
+    System.out.println("Using Comparable(SelectionSort)");
+    System.out.print("Unsorted: ");
+    for (Integer i : arr) {
+      System.out.print(i);
+      System.out.print(" ");
+    }
+
+    System.out.print("\nSorted:");
+
+    SelectionSort.sort(arr);
+    for (Integer i : arr) {
+      System.out.print(i);
+      System.out.print(" ");
+    }
 
   }
 
