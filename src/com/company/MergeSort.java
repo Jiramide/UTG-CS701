5958d7ab9ab95ff819d21abd568107ae8caacb4f
Jiramide <jadepiramidepogi@gmail.com>
2019-04-13 16:06:47 -0700

    Added MergeSort merging.

---
+++ src/com/company/MergeSort.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.lang.Comparable;
+
+public class MergeSort {
+
+  public static <E> void merge(E[] arr, Comparator<E> comp, int i, int j, int k) {
+    ArrayList<E> left = new ArrayList<E>(j - i + 1);
+    ArrayList<E> right = new ArrayList<E>(k - j + 1);
+
+    for (int idx = i; idx < j; idx++) {
+      left.add(arr[idx]);
+    }
+
+    for (int idx = j; idx < k; idx++) {
+      right.add(arr[idx]);
+    }
+
+    int idx = i;
+    while (!left.isEmpty() && !right.isEmpty()) {
+      int compRes = comp.compare(left.get(0), right.get(0));
+
+      if (compRes < 0) {
+        arr[idx] = left.remove(0);
+      } else {
+        arr[idx] = right.remove(0);
+      }
+
+      idx += 1;
+    }
+
+    while (!left.isEmpty()) {
+      arr[idx++] = left.remove(0);
+    }
+
+    while (!right.isEmpty()) {
+      arr[idx++] = right.remove(0);
+    }
+
+  }
+
+}
