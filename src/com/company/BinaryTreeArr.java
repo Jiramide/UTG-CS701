1abe69ed29a7276f709e58b20486b94a76b961b2
Jade Piramide <jadepiramidepogi@gmail.com>
2019-05-12 01:29:18 -0700

    Added index getter methods and preOrder traversal.

--- src/com/company/BinaryTreeArr.java
+++ src/com/company/BinaryTreeArr.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Comparator;
+import java.util.function.Consumer;
 
 public class BinaryTreeArr<E> extends ArrStruct<E> {
 
@@ -22,6 +23,31 @@ public class BinaryTreeArr<E> extends ArrStruct<E> {
 
   public static <E extends Comparable<E>> BinaryTreeArr<E> withComparable(Class<E> cls, int capacity) {
     return new BinaryTreeArr<>(cls, capacity, (x, y) -> x.compareTo(y));
+  }
+
+  private int getLeftChildIdx(int parent) {
+    return 2*parent + 1;
+  }
+
+
+
+  private int getRightChildIdx(int parent) {
+    return 2*parent + 2;
+  }
+
+  private int getParentIdx(int child) {
+    return (int)Math.floor((child - 1)/2);
+  }
+
+  // preorder: NLR
+  // in-order: LNR
+  // out-order: RNL
+  // postorder: LRN
+
+  public void preOrder(int curr, Consumer<E> process) {
+    process.accept(container[curr]);
+    preOrder(getLeftChildIdx(curr), process);
+    preOrder(getRightChildIdx(curr), process);
   }
 
 }
