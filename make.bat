bef77f1352fb7b80e68cefeec917a5468a032225
Jade Piramide <jadepiramidepogi@gmail.com>
2019-05-25 00:50:16 -0700

    Removed unnecessary bindings within make.bat.

--- make.bat
+++ make.bat
@@ -1,10 +1,9 @@
 @echo off
 set lang=%1
-set makeArgs=%2
 
 echo [%date% %time%] %lang% Makefile running: 
 
-cd src/%lang% && mingw32-make %makeArgs% && ^
+cd src/%lang% && mingw32-make %2 && ^
 echo [%date% %time%] %lang% Makefile complete. || ^
 echo [%date% %time%] %lang% Makefile failed.
 
