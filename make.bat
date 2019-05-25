367b1e62905d4844a3cae8231af48353bb4af3c7
Jade Piramide <jadepiramidepogi@gmail.com>
2019-05-24 20:03:00 -0700

    Added more onto make.bat as well as formatted it.

--- make.bat
+++ make.bat
@@ -1,7 +1,11 @@
 @echo off
 set /p lang=Input which language directory to make: 
 set /p makeArgs=Makefile arguments: 
+
 echo [%date% %time%] %lang% Makefile running: 
-cd src/%lang% && mingw32-make %makeArgs%
+
+cd src/%lang% && mingw32-make %makeArgs% && ^
+echo [%date% %time%] %lang% Makefile complete. || ^
+echo [%date% %time%] %lang% Makefile failed.
+
 cd ../..
-echo [%date% %time%] %lang% Makefile complete.
