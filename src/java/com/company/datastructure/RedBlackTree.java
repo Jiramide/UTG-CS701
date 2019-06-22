876c5920d3e529ff9d7e29ed9c544d63d7503d40
Jade Piramide <jadepiramidepogi@gmail.com>
2019-06-22 06:56:20 -0700

    Added RedBlackTree as an object to make within the Makefile as well as some more of RedBlackTree's implementation.

--- src/java/com/company/datastructure/RedBlackTree.java
+++ src/java/com/company/datastructure/RedBlackTree.java
@@ -1,6 +1,33 @@
 package com.company.datastructure;
 
+enum Colour {
+  BLACK,
+  RED;
+
+  public Colour toggle() {
+    return this == Colour.RED
+      ? Colour.BLACK
+      : Colour.RED;
+  }
+}
+
+class RedBlackTreeNode<E> {
+
+  E val;
+  Colour col;
+  RedBlackTreeNode<E> parent;
+  RedBlackTreeNode<E> left;
+  RedBlackTreeNode<E> right;
+
+  RedBlackTreeNode(E val) {
+    this.val = val;
+    this.col = Colour.BLACK;
+  }
+
+}
+
 public class RedBlackTree<E> extends BinaryTree<E> {
 
+  RedBlackTreeNode<E> root;
 
 }
