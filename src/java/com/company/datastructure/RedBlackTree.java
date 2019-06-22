1e7293de1a028225893930a631b82e5573f08c08
Jade Piramide <jadepiramidepogi@gmail.com>
2019-06-22 09:09:40 -0700

    Finished RedBlackTree.insert, complete with structure repairing (to maintain a balanced tree).

--- src/java/com/company/datastructure/RedBlackTree.java
+++ src/java/com/company/datastructure/RedBlackTree.java
@@ -21,25 +21,76 @@ public class RedBlackTree<E> extends BinaryTree<E> {
 
     RedBlackTree.Colour col;
     Node<E> parent;
+    Node<E> left;
+    Node<E> right;
 
     Node(E val) {
       super(val);
       this.col = Colour.RED;
     }
 
-    void setLeftChild(RedBlackTree<E> child) {
+    void setLeftChild(Node<E> child) {
       left = child;
       child.parent = this;
     }
 
-    void setRightChild(RedBlackTree<E> child) {
+    void setRightChild(Node<E> child) {
       right = child;
       child.parent = this;
     }
 
+    static RedBlackTree.Colour getCol(Node<?> node) {
+      return node != null
+        ? node.col
+        : RedBlackTree.Colour.BLACK;
+    }
+
+    Node<E> getGrandparent() {
+      return parent != null
+        ? parent.parent
+        : null;
+    }
+
+    Node<E> getUncle() {
+      if (parent == null || parent.parent == null) {
+        return null;
+      }
+
+      Node<E> grandparent = getGrandparent();
+      return parent == grandparent.left
+        ? grandparent.right
+        : grandparent.left;
+    }
+
+    RedBlackTree.Colour grandparentColour() {
+      return Node.getCol(getGrandparent());
+    }
+
+    RedBlackTree.Colour uncleColour() {
+      return Node.getCol(getUncle());
+    }
+
+    void leftRotate() {
+      Node<E> oldRight = right;
+
+      right = right.left;
+      oldRight.parent = parent;
+      parent = oldRight;
+      oldRight.left = this;
+    }
+
+    void rightRotate() {
+      Node<E> oldLeft = left;
+
+      left = left.right;
+      oldLeft.parent = parent;
+      parent = oldLeft;
+      oldLeft.right = this;
+    }
+
   }
 
-  RedBlackTree.Node<E> root;
+  protected RedBlackTree.Node<E> root;
 
   public RedBlackTree(Comparator<? super E> comp) {
     super(comp);
@@ -49,7 +100,7 @@ public class RedBlackTree<E> extends BinaryTree<E> {
     super(iter, comp);
   }
 
-  public BinaryTree(Iterable<? extends E> container, Comparator<? super E> comp) {
+  public RedBlackTree(Iterable<? extends E> container, Comparator<? super E> comp) {
     this(container.iterator(), comp);
   }
 
@@ -79,13 +130,64 @@ public class RedBlackTree<E> extends BinaryTree<E> {
         : curr.right;
     }
 
+    RedBlackTree.Node<E> newNode = new RedBlackTree.Node<>(val);
+
     if (prev == null) {
-      root = new RedBlackTree.Node<>(val);
+      root = newNode;
     } else if (comp.compare(val, prev.val) < 0) {
-      prev.left = new RedBlackTree.Node<>(val);
+      prev.setLeftChild(newNode);
     } else {
-      prev.right = new RedBlackTree.Node<>(val);
+      prev.setRightChild(newNode);
     }
+  }
+
+  private boolean continueRepair(RedBlackTree.Node<E> node) {
+    return Node.getCol(node.parent) != RedBlackTree.Colour.BLACK
+      && node != root;
+  }
+
+  private void repair(RedBlackTree.Node<E> node) {
+    Node<E> uncle = node.getUncle();
+    while (Node.getCol(uncle) == RedBlackTree.Colour.RED
+      && continueRepair(node)) {
+      uncle.col = uncle.col.toggle();
+      node.parent.col = node.parent.col.toggle();
+
+      Node<E> grandparent = node.getGrandparent();
+      grandparent.col = RedBlackTree.Colour.RED;
+
+      node = grandparent;
+    }
+
+    // uncle.col == BLACK => uncle.col == BLACK || uncle == null
+    // rotate
+    if (continueRepair(node)) {
+      Node<E> parent = node.parent;
+      Node<E> grandparent = node.getGrandparent();
+
+      if (parent == grandparent.left) {
+        if (node == parent.right) {
+          // left right
+          parent.leftRotate();
+        }
+
+        grandparent.rightRotate();
+      } else {
+        if (node == parent.left) {
+          // right left
+          parent.rightRotate();
+        }
+
+        grandparent.leftRotate();
+      }
+
+      RedBlackTree.Colour gCol = grandparent.col;
+      grandparent.col = parent.col;
+      parent.col = gCol;
+    }
+
+    // finished repairing, set root to black
+    root.col = RedBlackTree.Colour.BLACK;
   }
 
 }
